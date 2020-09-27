3314a14f19fa80ed8f16a6f0d41e7ad2051df77e
Dev <devvmashru@protonmail.ch>
2020-09-27 18:15:30 +0530

    python: Allow user to select the unit
    Signed-off-by: Dev <devvmashru@protonmail.ch>

--- python/download.py
+++ python/download.py
@@ -11,10 +11,10 @@
 	Insert path to the firefox profile you configured. (Note: A firefox profile with the pre-requisites has to be made in order for the script to run properly)
 
 	Run as follows:
-	python download.py <position_of_course_in_list> <starting_lecture_number> <ending_lecture_number>
+	python download.py <position_of_course_in_list> <unit_number> <starting_lecture_number> <ending_lecture_number>
 	example:
-	python download.py 3 5 12
-	Running the above code downloads lectures 5-12(both inclusive) of the 3rd course in 'My Courses' list
+	python download.py 3 2 5 12
+	Running the above code downloads lectures 5-12(both inclusive) of the 2nd unit of the 3rd course in 'My Courses' list
 """
 
 import time, sys
@@ -38,11 +38,13 @@ def download(args):
 	pw.send_keys(password)
 	driver.find_element_by_id('postloginform#/Academy/j_spring_security_check').click()
 
-	for i in range(int(args[2]),int(args[3])+1):
+	for i in range(int(args[3]),int(args[4])+1):
 		time.sleep(2)
 		driver.find_element_by_id('menuTab_653').click()																							# Click 'My courses'
 		time.sleep(2)
 		driver.find_element_by_xpath('/html/body/div[3]/div[2]/div/div/div[2]/div/div[3]/div/table/tbody/tr[' + args[1] + ']/td[2]').click()		# Click course name
+		time.sleep(2)
+		driver.find_element_by_xpath('/html/body/div[3]/div[2]/div/div[2]/div[2]/div[1]/div/ul/li[' + args[2] + ']').click()						# Click on the desired unit
 		time.sleep(2)
 		driver.find_element_by_xpath('/html/body/div[3]/div[2]/div/div[2]/div[2]/div[1]/div/div/div/div[' + str(i) +']').click()					# Click lecture number
 		time.sleep(2)
