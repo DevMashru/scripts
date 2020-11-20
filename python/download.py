51ab58f00f0d2d2d0300296913676accf8a11e90
Dev <devvmashru@protonmail.ch>
2020-11-20 16:03:57 +0530

    Updated to work with the redesign

--- python/download.py
+++ python/download.py
@@ -11,9 +11,9 @@
 	Insert path to the firefox profile you configured. (Note: A firefox profile with the pre-requisites has to be made in order for the script to run properly)
 
 	Run as follows:
-	python download.py <position_of_course_in_list> <unit_number> <slides_or_notes> <starting_lecture_number> <ending_lecture_number>	(pass 2 to download slides, 3 to download notes)
+	python download.py <position_of_course_in_list> <unit_number> <slides_or_notes> <starting_lecture_number> <ending_lecture_number>	(pass 3 to download slides, 4 to download notes)
 	example:
-	python download.py 3 2 2 5 12
+	python download.py 3 2 3 5 12
 	Running the above code downloads slides of lectures 5-12(both inclusive) of the 2nd unit of the 3rd course in 'My Courses' list
 """
 
@@ -40,15 +40,15 @@ def download(args):
 
 	for i in range(int(args[4]),int(args[5])+1):
 		time.sleep(2)
-		driver.find_element_by_id('menuTab_653').click()																							# Click 'My courses'
+		driver.find_element_by_id('menuTab_653').click()																									# Click 'My courses'
 		time.sleep(2)
-		driver.find_element_by_xpath('/html/body/div[3]/div[2]/div/div/div[2]/div/div[3]/div/table/tbody/tr[' + args[1] + ']/td[2]').click()		# Click course name
+		driver.find_element_by_xpath('/html/body/div[3]/div[2]/div/div[3]/div[2]/div/div[3]/div/div[1]/table/tbody/tr[' + args[1] + ']/td[2]').click()		# Click course name
 		time.sleep(2)
-		driver.find_element_by_xpath('/html/body/div[3]/div[2]/div/div[2]/div[2]/div[1]/div/ul/li[' + args[2] + ']').click()						# Click on the desired unit
+		driver.find_element_by_xpath('/html/body/div[3]/div[2]/div/div[2]/div/div[1]/div/ul/li[' + args[2] + ']/a').click()									# Click on the desired unit
 		time.sleep(2)
-		driver.find_element_by_xpath('/html/body/div[3]/div[2]/div/div[2]/div[2]/div[1]/div/div/div/div[' + str(i) +']').click()					# Click lecture number
+		driver.find_element_by_xpath('/html/body/div[3]/div[2]/div/div[2]/div/div[1]/div/div/div/div[1]/table/tbody/tr[' + str(i) +']/td[1]').click()		# Click lecture number
 		time.sleep(2)
-		driver.find_element_by_xpath('/html/body/div[3]/div[2]/div/div[2]/ul/li[' + args[3] + ']/a').click()										# Click 'slides' or 'notes' tab depending on user input
+		driver.find_element_by_xpath('/html/body/div[3]/div[2]/div/div[2]/ul/li[' + args[3] + ']/a').click()												# Click 'slides' or 'notes' tab depending on user input
 
 if __name__ == "__main__":
 	download(sys.argv)
